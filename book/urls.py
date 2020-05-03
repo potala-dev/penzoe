19dd211965b2469fac468e192205cdb2cf6d3d3f
kaldan <kaldantenzin@gmail.com>
2020-05-03 19:07:31 +0530

    list by category completed

--- book/urls.py
+++ book/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.book_list, name='home'),
-    path('book/<id>/', views.book_detail, name='book_detail')
+    path('book/<int:id>/', views.book_detail, name='book_detail'),
+    path('Category/<cat>/', views.book_by_cat, name='book_by_cat'),
 ]
--- book/views.py
+++ book/views.py
@@ -4,21 +4,39 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Book
 # Create your views here.
 
-def book_list(request):
+def book_list(request,):
 
-    all_books = Book.objects.order_by('rank')
-    paginator = Paginator(all_books, 3)
-    page = request.GET.get('page')
+    all_books = Book.objects.order_by('-rank')
+    paginator = Paginator(all_books, 1)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
     try:
-        books = paginator.page(page)
+        paginated_queryset = paginator.page(page)
     except PageNotAnInteger:
-        books = paginator.page(1)
+        paginated_queryset = paginator.page(1)
     except EmptyPage:
-        books = paginator.page(paginator.num_pages)
-    context = {'page': page, 'books': books }
+        paginated_queryset = paginator.page(paginator.num_pages)
+    context = {'page': page_request_var, 'books': paginated_queryset }
     return render(request, 'book/book_list.html', context)
 
 def book_detail(request, id):
     book = Book.objects.filter(pk=id)
     print(book)
     return render(request, 'book/book_details.html', {'book': book[0]})
+
+def book_by_cat(request, cat):
+    if cat:
+        all_books = Book.objects.filter(genre=cat).order_by('-rank')
+    else:
+        all_books = Book.objects.order_by('-rank')
+    paginator = Paginator(all_books, 1)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+    context = {'page': page_request_var, 'books': paginated_queryset }
+    return render(request, 'book/book_list.html', context)
