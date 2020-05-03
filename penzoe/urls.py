677822b95952ffdca78a23a1c0a89ea88d8e2967
tenzin <ten13zin@gmail.com>
2020-05-03 18:43:34 +0530

    fix: redirect to home after password change

--- penzoe/urls.py
+++ penzoe/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from allauth.account.views import PasswordChangeView
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    success_url = '/'
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('book.urls')),
-    #path('account/', include('account.urls'))
+    path('accounts/password/change/',
+        CustomPasswordChangeView.as_view(),
+        name="account_password_change"),
     path('accounts/', include('allauth.urls'))
 ]
---
+++ resetdb.sh
@@ -0,0 +1,6 @@
+find . -name "*.pyc" -exec rm {} \;
+rm db.sqlite3
+
+python manage.py migrate
+
+# python manage.py create_test_data
