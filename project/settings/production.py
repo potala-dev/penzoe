f15d15942b597c76b1f883ae58ae2e95beb8570a
tenzin <ten13zin@gmail.com>
2021-05-22 20:07:40 +0530

    fix: heroku deploy

--- project/settings/production.py
+++ project/settings/production.py
@@ -1,13 +1,13 @@
 import django_heroku
 
-from .base import *
+from .base import *  # noqa: F403
 
 # Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
+EMAIL_HOST_USER = env("EMAIL_USER")  # noqa: F405
+EMAIL_HOST_PASSWORD = env("EMAIL_PASS")  # noqa: F405
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), databases=False)
