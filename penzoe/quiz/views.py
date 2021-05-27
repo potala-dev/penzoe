2ea40384092585f366cbfd775e4f8acaa5088311
tenzin <ten13zin@gmail.com>
2021-05-27 22:23:03 +0530

    add: vue js for fetch mcq

--- penzoe/quiz/views.py
+++ penzoe/quiz/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 
-from .utils import get_sentence_with_correct_spelling_choices
+# from .utils import get_sentence_with_correct_spelling_choices
 
 
 def get_spelling_mcq(request, level):
-    lelft_context, right_context, choices = get_sentence_with_correct_spelling_choices(
-        level
-    )
+    # lelft_context, right_context, choices = get_sentence_with_correct_spelling_choices(
+    #     level
+    # )
 
-    context_data = {
-        "left_context": lelft_context,
-        "right_context": right_context,
-        "choices": choices,
-    }
+    # context_data = {
+    #     "left_context": lelft_context,
+    #     "right_context": right_context,
+    #     "choices": choices,
+    # }
 
-    return render(request, "quiz/mcq.html", context_data)
+    return render(request, "quiz/mcq.html", {"level": level})
