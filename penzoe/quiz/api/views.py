77231ca49fa4452750130daf6dcbfa34e9f8da78
tenzin <ten13zin@gmail.com>
2021-05-29 11:39:53 +0530

    fix: add default for level param

--- penzoe/quiz/api/views.py
+++ penzoe/quiz/api/views.py
@@ -16,7 +16,7 @@ class SpellFBMCQView(views.APIView):
             right_context,
             choices,
             correct_choice,
-        ) = get_sentence_with_correct_spelling_choices(request.GET["level"])
+        ) = get_sentence_with_correct_spelling_choices(request.GET.get("level", "C1"))
 
         data = {
             "left_context": lelft_context,
